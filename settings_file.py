5a32be9e37924ebf0f0b88e44e40d1220922179c
Matt <mblanchard23@gmail.com>
2015-11-19 02:05:03 +0000

    Settings file

---
+++ settings_file.py
@@ -0,0 +1,7 @@
+settingsdict = {"username":"", #Your SF Username
+					"password":"", #Your SF Password
+					"b64_password":"",  #Optional enter your pw in b64 to stop people snooping over your shoulder
+					"token":"", #Your SF security token
+					"sqlitedirectory":"",
+					"instance":""}
+
