4736ac4613834f4c987cf1ae4402e86fcf36c657
Matt <mblanchard23@gmail.com>
2016-03-21 11:19:32 +0000

    Removed instance input (now defeunct) and added comments

--- settings_file.py
+++ settings_file.py
@@ -2,6 +2,6 @@ settingsdict = {"username":"", #Your SF Username
 					"password":"", #Your SF Password
 					"b64_password":"",  #Optional enter your pw in b64 to stop people snooping over your shoulder
 					"token":"", #Your SF security token
-					"sqlitedirectory":"",
-					"instance":""}
+					"sqlitedirectory":"" #Enter a directory to store the logger file - best to just use the dir sfbulkapi is located,
+					}
 
